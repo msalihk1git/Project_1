11983c8556cd1f507da7587fce9100cd18c3fd97
msalihk1git <msalihk1@gmail.com>
2022-07-25 00:32:48 +0530

    update

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-import pickle 
+#import relevant libraries for flask,html rendering and loading the ML model
+from flask import Flask,request,url_for,render_template
+import pickle
 import pandas as pd
-
-app=Flask(__name__)
-
-
-model= pickle.load(open("model.pk1","rb"))
-scale=pickle.load(open("scale.pk1","rb"))
-
-
+import joblib
+app = Flask(__name__)
+# model=pickle.load(open("model.pkl","rb"))
+model=joblib.load(open("model.pkl","rb"))
+# scale=pickle.load(open("scale.pkl","rb"))
+scale=joblib.load(open("scale.pkl","rb"))
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    return render_template("index.html") 
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
