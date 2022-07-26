a77b2f68963ed3b9eaf2156897e1df65e457bd99
msalihk1git <msalihk1@gmail.com>
2022-07-26 12:16:41 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #import relevant libraries for flask,html rendering and loading the ML model
+from msilib.schema import InstallUISequence
 from flask import Flask,request,url_for,render_template
 import pickle
 import pandas as pd
@@ -8,10 +9,30 @@ app = Flask(__name__)
 model=joblib.load(open("model.pkl","rb"))
 # scale=pickle.load(open("scale.pkl","rb"))
 scale=joblib.load(open("scale.pkl","rb"))
+
+
 @app.route("/")
-def hello_world():
+def landingPage():
+    return render_template("index.html") 
+
+@app.route("/predict",methods=["POST"])
+def predict():
+    pregnancies = request.form['1']
+    glucose = request.form['2']
+    bloodPressure = request.form['3']
+    skinThickness = request.form['4']
+    insulin = request.form['5']
+    bmi = request.form['6']
+    dpf = request.form['7']
+    age = request.form['8']
+    rowDf=pd.DataFrame([pd.Series([pregnancies,glucose,bloodPressure,skinThickness,insulin,bmi,dpf,age])])
+    rowDf_new=pd.DataFrame(scale.transform(rowDf))
+    
+    print(rowDf_new)
+
     return render_template("index.html") 
 
 if __name__ == "__main__":
     app.run(debug=True)
     
+
